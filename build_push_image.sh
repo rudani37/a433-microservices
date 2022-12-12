61b92917129be6d77776b25f4b6e31961e8fa62d
Rudani <rudani097@gmail.com>
2022-12-12 15:12:46 +0700

    update

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-TOKEN=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  
-
 docker build -t item-app:v1 . # Perintah untuk membuat Docker image, nama image item-app, dan memiliki tag v1.
 
 docker images # Melihat daftar image di lokal
 
-docker tag 42be613db468 ghcr.io/rudani37/item-app:v1 # Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag item-app:v1 ghcr.io/rudani37/item-app:v1 # Mengubah nama image agar sesuai dengan format GitHub Packages
 
 echo $TOKEN | docker login ghcr.io -u rudani37 --password-stdin # Login ke GitHub Packages via Terminal
 
