bedc6f3d4910bad7e6f053710e79c17f8c31230f
Rudani <rudani097@gmail.com>
2022-12-13 08:11:40 +0700

    update

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker build -t item-app:v1 . # Perintah untuk membuat Docker image, nama image item-app, dan memiliki tag v1.
+docker build -t item-app:v1 .                                   # Perintah untuk membuat Docker image, nama image item-app, dan memiliki tag v1.
 
-docker images # Melihat daftar image di lokal
+docker images                                                   # Melihat daftar image di lokal
 
-docker tag item-app:v1 ghcr.io/rudani37/item-app:v1 # Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag item-app:v1 ghcr.io/rudani37/item-app:v1             # Mengubah nama image agar sesuai dengan format GitHub Packages
 
 echo $TOKEN | docker login ghcr.io -u rudani37 --password-stdin # Login ke GitHub Packages via Terminal
 
-docker push ghcr.io/rudani37/item-app:v1set # Mengunggah image ke GitHub Packages
+docker push ghcr.io/rudani37/item-app:v1set                     # Mengunggah image ke GitHub Packages
