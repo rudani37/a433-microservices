4fc5098edf53642f3013bc19539a462855f076df
Rudani <rudani097@gmail.com>
2022-12-25 23:20:48 +0700

    update circleci config

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker build -t karsajobs-ui:latest .                                   # Perintah untuk membuat Docker image, nama image item-app, dan memiliki tag v1.
+docker build -t karsajobs-ui:latest .                                   # Perintah untuk membuat Docker image, nama image karsajobs-ui, dan memiliki tag latest.
 
-docker tag karsajobs-ui:latest ghcr.io/rudani37/karsajobs-ui:latest             # Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag karsajobs-ui:latest ghcr.io/rudani37/karsajobs-ui:latest     # Mengubah nama image agar sesuai dengan format GitHub Packages
 
-read -p "Token: " TOKEN
+read -p "Token: " TOKEN                                                 # membaca inputan token dari ketika di terminal
 
-echo $TOKEN | docker login ghcr.io -u rudani37 --password-stdin # Login ke GitHub Packages via Terminal
+echo $TOKEN | docker login ghcr.io -u rudani37 --password-stdin         # Login ke GitHub Packages via Terminal
 
 docker push ghcr.io/rudani37/karsajobs-ui:latest                        # Mengunggah image ke GitHub Packages
